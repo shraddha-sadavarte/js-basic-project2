d3228b383acdc6dc0679e49d31c3452147802ee3
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2024-06-12 16:48:32 +0530

    full js added

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 window.addEventListener("mousemove", function(details){
     //console.log(details.clientX);
+
+    var rect = document.querySelector("#rect")
+    var xval = gsap.utils.mapRange(
+        0, 
+        window.innerWidth, 
+        100+rect.getBoundingClientRect().width/2, //aadha card andar aayega + 100 points aur andar aa jayega  
+        window.innerWidth - (100+rect.getBoundingClientRect().width/2), 
+        details.clientX)
+
     gsap.to('#rect', {
-        left: details.clientX+"px",
+        left: xval+"px",
         ease:Power3,
     })
 })
